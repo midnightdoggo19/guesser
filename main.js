44bb1093c68446baa144e050b6fbaf8e98f0cd33
midnightdoggo19 <71900479+midnightdoggo19@users.noreply.github.com>
2024-11-17 19:01:32 +0000

    Updated main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ client.on('messageCreate', async (message) => {
             return;
         }
 
-        await message.reply(`Archiving messages from ${message.channel.name}...`);
+        randomReact(message);
 
         try {
             let messages = [];
@@ -110,13 +110,13 @@ client.on('messageCreate', async (message) => {
     else if (message.content.toLowerCase() === '!retrain') {
         logger.info(`Retrain command received in channel ${message.channel.name} by ${message.author.username}`);
         try {
-            randomReact(message)
+            randomReact(message);
             const retrain = spawn('python3', ['./python/train.py']); // Run a python script to retrain the model
             retrain.stdout.on('data', data => {
                 console.log(data.toString()); // Log output from python
             });
             retrain.on('close', async code => { // When python finishes
-                await message.reply('Finished training model!')
+                await message.react(':white_check_mark:')
                 logger.info('Model training finished!')
             });
         }
