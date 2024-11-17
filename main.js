1eeee9144a0e95a91025624bd7b47d2935203bda
Midnight Doggo <71900479+midnightdoggo19@users.noreply.github.com>
2024-11-17 21:41:34 +0000

    Made I i
    lowercased

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ client.on('messageCreate', async (message) => {
         predictor.on('close', async code => {
             if (code !== 0) {
                  logger.error(`Python script exited with code ${code}`);
-                 await message.reply('**Sorry, there was an error making the prediction.**\nPlease try running \`!retrain\`. If that fails, please try \`!archive\`. If that fails, please [open a GitHub Issue](<https://github.com/midnightdoggo19/guesser/issues/new>).');
+                 await message.reply('**Sorry, there was an error making the prediction.**\nPlease try running \`!retrain\`. If that fails, please try \`!archive\`. If that fails, please [open a GitHub issue](<https://github.com/midnightdoggo19/guesser/issues/new>).');
             } else {
                  const predictedUser = prediction.trim();
                  logger.info(`Predicted user for message "${message.content}": ${predictedUser}`);
